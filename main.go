f276185ac55939f48d7292bf22955fc798142f16
Markus <fischbacher.markus@gmail.com>
2017-03-10 18:33:34 +0100

    test 4

--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/rockaut/g2z"
 )
 
 func main() {
 	fmt.Println("Say hello HPE 3Par")
+}
+
+func Echo(request *g2z.AgentRequest) (string, error) {
+	return strings.Join(request.Params, " "), nil
+}
+
+func init() {
+	g2z.RegisterStringItem("go.echo", "Hello world!", Echo)
 }
