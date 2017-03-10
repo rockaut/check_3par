5fbf4252fd3fe0939170b7073ed912a5e81247eb
Markus <fischbacher.markus@gmail.com>
2017-03-10 17:24:12 +0100

    test 3

--- helpers/build-container/build.sh
+++ helpers/build-container/build.sh
@@ -2,4 +2,4 @@ mkdir ~/src
 
 git clone -b patch-1 https://github.com/rockaut/g2z.git ~/src/github.com/rockaut/g2z
 
-docker run --rm -v "$PWD"/src:/src -w /src/github.com/rockaut/check_3par golang:latest go build -v
+docker run --rm -v "$PWD"/src:/go/src -w /go/src/github.com/rockaut/check_3par golang:latest go build -v
