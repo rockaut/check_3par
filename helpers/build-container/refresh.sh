0ab1c6515dd4c49e16acb95dd8d4f60e29f561d1
Markus <fischbacher.markus@gmail.com>
2017-03-10 20:09:30 +0100

    added cp

--- helpers/build-container/refresh.sh
+++ helpers/build-container/refresh.sh
@@ -2,4 +2,7 @@ cd ~/src/github.com/rockaut/check_3par
 git pull
 docker run --rm -v /root/bin:/go/bin -v /root/src:/go/src -w /go/src/github.com/rockaut/check_3par golang:latest go build -buildmode=c-shared -v -o /go/bin/check_3par.so
 
+cp ~/bin/check_3par.so /graph/volumes/zabbixcontainer_zabbix_agent_modules/_data/
+cp ~/bin/check_3par.so /graph/volumes/zabbixcontainer_zabbix_modules/_data/
+
 cd -
