f088270b5a76bd75665796ef831aa75408c9d559
Markus <fischbacher.markus@gmail.com>
2017-03-10 20:53:08 +0100

    test 7

--- helpers/build-container/refresh.sh
+++ helpers/build-container/refresh.sh
@@ -1,3 +1,5 @@
+docker-compose down
+
 cd ~/src/github.com/rockaut/check_3par
 git pull
 docker run --rm -v /root/bin:/go/bin -v /root/src:/go/src -w /go/src/github.com/rockaut/check_3par golang:latest go build -buildmode=c-shared -v -o /go/bin/check_3par.so
@@ -6,3 +8,5 @@ cp ~/bin/check_3par.so /graph/volumes/zabbixcontainer_zabbix_agent_modules/_data
 cp ~/bin/check_3par.so /graph/volumes/zabbixcontainer_zabbix_modules/_data/
 
 cd -
+
+docker-compose up -d
